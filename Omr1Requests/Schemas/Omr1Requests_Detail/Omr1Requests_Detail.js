1b6db2caadb98d0b04ac8497e25a555e7a8602ac
omar151ismael <ismaelomar151@yahoo.com>
2026-08-24 15:06:56 +0300

    Initial commit

---
+++ Omr1Requests/Schemas/Omr1Requests_Detail/Omr1Requests_Detail.js
@@ -0,0 +1,8 @@
+define('Omr1Requests_Detail', [], function () {
+	return {
+		entitySchemaName: 'Omr1Requests',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
